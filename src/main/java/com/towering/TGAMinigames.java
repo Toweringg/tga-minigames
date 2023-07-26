1db504be02b11eb67f7b1c51d05126f3b3f15c1b
DJ <djg325@gmail.com>
2023-07-25 23:31:22 -0400

    TGA Minigames

--- src/main/java/com/towering/TGAMinigames.java
+++ src/main/java/com/towering/TGAMinigames.java
@@ -1,7 +1,6 @@
 package com.towering;
 
-import com.towering.SoundsManager.SoundsCommands;
-import com.towering.SoundsManager.SoundsEvents;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -9,14 +8,17 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerResourcePackStatusEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+
 public final class TGAMinigames extends JavaPlugin implements Listener{
 
     @Override
     public void onEnable() {
+        saveDefaultConfig();
         getLogger().info("TGAMinigames has been enabled!");
         getServer().getPluginManager().registerEvents(this, this);
-        getServer().getPluginManager().registerEvents(new SoundsEvents(), this);
-        getCommand("music").setExecutor(new SoundsCommands());
     }
 
     @Override
@@ -35,9 +37,6 @@ public final class TGAMinigames extends JavaPlugin implements Listener{
 
         if (packStatus == PlayerResourcePackStatusEvent.Status.DECLINED || packStatus == PlayerResourcePackStatusEvent.Status.FAILED_DOWNLOAD){
             player.kickPlayer("Texture pack error.");
-        }
-        if (packStatus == PlayerResourcePackStatusEvent.Status.SUCCESSFULLY_LOADED)  {
-            player.playSound(player.getLocation(), "audio2", 0.2f, 1f);
         }
     }
 }
